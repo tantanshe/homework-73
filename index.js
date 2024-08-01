2e45b3222366e71803b7b037e849a94ebeb167b6
Tatiana Shishenina <tatyana020711@gmail.com>
2024-08-01 23:45:32 +0600

    Finished task number 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+const password = "password"
 
 const app = express();
 const port = 8000;
@@ -6,6 +8,18 @@ const port = 8000;
 app.get('/:echo', (req, res) => {
     const echo = req.params.echo;
     return res.send(echo);
+})
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encodedText = Vigenere.Cipher(password).crypt(text);
+    return res.send(encodedText);
+})
+
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decodedText = Vigenere.Decipher(password).crypt(text);
+    return res.send(decodedText);
 })
 
 app.listen(port, () => {
